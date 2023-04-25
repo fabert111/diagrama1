1e58b310c9d3a94c2ff0b46d1712adb767ed13f4
faber <esneidertinoco111@gmail.com>
2023-04-25 13:01:02 -0500

    diagrama

---
+++ WORKBOOK CONDICIONALES IF , ELSE Y CICLOS FOR PYTHON.py
@@ -0,0 +1,8 @@
+#Pista: la mayoría de edad en Colombia es de 18 años
+edad = int(input("Por favor, ingrese su edad: "))
+
+# Verificación si es mayor de edad o no
+if edad >= 18:
+    print("¡Chimba usted es mayor de edad  😁 !")
+else:
+    print("Pailas, no es mayor de edad , pala casa 😔 .")
---
+++ par_o_impar.py
@@ -0,0 +1,8 @@
+# Ingreso del número por parte del usuario
+numero = int(input("Por favor, ingrese un número entero: "))
+
+# Verificación si el número es par o impar
+if numero % 2 == 0:
+    print("El número ingresado es par.")
+else:
+    print("El número ingresado es impar.")
